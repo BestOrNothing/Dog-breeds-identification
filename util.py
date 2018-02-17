b5b2a8f7ef5544eb505009de76ab7d5975959f7c
BestOrNothing <1562490789@qq.com>
2018-02-17 15:01:29 +0000

    modify util.py

--- util.py
+++ util.py
@@ -1,15 +1,22 @@
 import numpy as np
 
-def flip_aug(input_data):
+def expand_to_times(input_data, times):
+    ret = input_data
+    for i in range(times - 1):
+        ret = np.vstack([ret, input_data])
+    return ret
+
+def flip_aug(input_data, label):
     """
     input_data: the data to be augmented
     return: augmented data set
     """
     flipped = np.flip(input_data, -2)
-    ret = np.vstack([input_data, flipped])
-    return ret
+    ret_x = np.vstack([input_data, flipped])
+    ret_label = expand_to_times(label, 2)
+    return [ret_x, ret_label]
 
-def rot_aug(input_data):
+def rot_aug(input_data, label):
     """
     input_data: the data to be augmented
     return: augmented data set
@@ -17,11 +24,8 @@ def rot_aug(input_data):
     rot1 = np.rot90(input_data, axes=[-3,-2])
     rot2 = np.rot90(rot1, axes=[-3,-2])
     rot3 = np.rot90(rot2, axes=[-3,-2])
-    ret = np.vstack([input_data, rot1, rot2, rot3])
-    return ret
+    ret_x = np.vstack([input_data, rot1, rot2, rot3])
+    ret_label = expand_to_times(label, 4)
+    return [ret_x, ret_label]
 
-def expand_to_times(input_data, times):
-    ret = input_data
-    for i in range(times - 1):
-        ret = np.vstack([ret, input_data])
-    return ret
+
